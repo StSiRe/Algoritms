3d9e696b2b1ef78572ae854cdac4f207b83306e6
Андрей Константинов <andy_hooli@outlook.com>
2022-08-08 13:02:38 +0300

    Написал сортировку расческой

---
+++ CombSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CombSort
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] array = { 12, 43, 2, 42, 43563, 212, 532, 543, 1234, 43 };
+            Console.WriteLine("Source array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            #region CombSort
+            double factor = 1.2473309;
+            int step = array.Length - 1;
+            while(step >= 1)
+            {
+                for (int i = 0; step + i < array.Length; i++)
+                {
+                    if(array[i] > array[i+step])
+                    {
+                        var t = array[i];
+                        array[i] = array[i + step];
+                        array[i + step] = t;
+                    }    
+                }
+                step = (int)(step / factor);
+            }
+            #endregion
+
+            Console.Write("Result:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
